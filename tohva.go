abbc26ad5c1eebb394f14026421a609499a619ed
Lucas Satabin <lucas.satabin@gnieh.org>
2012-11-30 15:56:24 +0100

    correctly handle post requests, sessions and cookies

--- tohva.go
+++ tohva.go
@@ -1,16 +1,14 @@
 package tohva
 
 import (
-  "fmt"
   "io"
   "io/ioutil"
   "encoding/json"
   "strconv"
-  "strings"
   "net/http"
   "net/url"
+  "strings"
   "log"
-  "time"
 )
 
 // this file contains the interface to the couchdb server
@@ -43,47 +41,61 @@ func CreateCouchClient(host string, port int) CouchDB {
 }
 
 // send a request that accepts a json and parse its result to the expected type
-func (couch *CouchDB) doJsonRequest(method string, path string, body io.Reader, result interface{}) error {
+func (couch *CouchDB) doJsonRequest(method string, path string, body io.Reader, form bool, result interface{}) error {
   req, err := couch.newRequest(method, path, body)
-  if err != nil {
-    return err
-  }
-  err = req.ParseForm();
   if err != nil {
     return err
   }
   // set the accept header
   req.Header.Add("Accept", "application/json")
+  // url encoded form submission
+  if form {
+    req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+    req.Header.Add("Referer", "http://localhost:5984")
+  }
+	if couch.client.Jar != nil {
+		for _, cookie := range couch.client.Jar.Cookies(req.URL) {
+			req.AddCookie(cookie)
+		}
+	}
   // send the request
   resp, err := couch.client.Do(req)
   if err != nil {
     return err
+  } else if err == nil && couch.client.Jar != nil {
+		couch.client.Jar.SetCookies(req.URL, resp.Cookies())
+	}
+  // don't forget to close the bodies
+  if body != nil {
+    defer req.Body.Close()
   }
+  defer resp.Body.Close()
   // parse the result
   respData, err := ioutil.ReadAll(resp.Body)
   if err != nil {
     return err
   }
-  return json.Unmarshal(respData, result)
+  err = json.Unmarshal(respData, result)
+  return err
 }
 
 // the cookie jar
 type cookieJar struct {
-  couchCookie *http.Cookie
+  couchCookie http.Cookie
 }
 
 // TODO make it thread safe
 func (jar *cookieJar) SetCookies(url *url.URL, cookies []*http.Cookie) {
   for i := range cookies {
     if cookies[i].Name == "AuthSession" {
-      jar.couchCookie = cookies[i]
+      jar.couchCookie = *cookies[i]
     }
   }
 }
 
 // TODO make it thread safe
 func (jar *cookieJar) Cookies(url *url.URL) []*http.Cookie {
-  return []*http.Cookie{jar.couchCookie}
+  return []*http.Cookie{&jar.couchCookie}
 }
 
 // start a new session for this couchdb instance
@@ -91,12 +103,12 @@ func (couch *CouchDB) StartSession() CouchSession {
   // add my personal cookie jar for this session
   // copy the underlying couch client
   my_couch := *couch
-  my_couch.client.Jar = &cookieJar{nil}
+  my_couch.client.Jar = &cookieJar{http.Cookie{}}
   return CouchSession { &my_couch, "" }
 }
 
 func (couch CouchDB) url() string {
-  return couch.Host + ":" + (strconv.Itoa(couch.Port) + "/")
+  return "http://" + couch.Host + ":" + (strconv.Itoa(couch.Port) + "/")
 }
 
 // get the base request for this couchdb instance
@@ -107,25 +119,59 @@ func (couch CouchDB) newRequest(method string, path string, body io.Reader) (*ht
 
 // ========== Couch Session ==========
 
+// Log user in (cookie based authentication)
 func (session *CouchSession) Login(name string, password string) bool {
   var r loginResult
-  body := strings.NewReader(fmt.Sprintf("name=%s&password=%s", name, password))
-  if err := session.doJsonRequest("POST", "_session", body, &r); err != nil {
-    log.Println("[ERROR]", time.Now(), err)
+  v := url.Values{}
+  v.Set("name", name)
+  v.Set("password", password)
+  body := strings.NewReader(v.Encode())
+  if err := session.doJsonRequest("POST", "_session", body, true, &r); err != nil {
+    log.Println("[ERROR]", err)
     return false
   }
 
   return r.Ok
 }
 
+// Log user out
 func (session *CouchSession) Logout() bool {
   var r simpleResult
-  if err := session.doJsonRequest("DELETE", "_session", nil, &r); err != nil {
-    log.Println("[ERROR]", time.Now(), err)
+  if err := session.doJsonRequest("DELETE", "_session", nil, false, &r); err != nil {
+    log.Println("[ERROR]", err)
     return false
   }
 
   return r.Ok
+}
+
+type SessionInfo struct {
+  Ok bool `json:"ok"`
+  Context *SessionContext `json:"userCtx"`
+  Info struct {
+    Authenticated *string `json:"authenticated,omitempty"`
+  } `json:"info"`
+}
+
+type SessionContext struct {
+  Name *string `json:"name"`
+  Roles []string `json:"roles"`
+}
+
+// Gets the current session context information
+func (session *CouchSession) GetSessionInfo() *SessionInfo {
+  var info SessionInfo
+  if err := session.doJsonRequest("GET", "_session", nil, false, &info); err != nil {
+    log.Println("[ERROR]", err)
+    return nil
+  }
+  return &info
+}
+
+// Is user logged in?
+func (session *CouchSession) IsLoggedIn() bool {
+  info := session.GetSessionInfo()
+  return info != nil && info.Info.Authenticated != nil
 }
 
 // ========== Database ==========
@@ -163,18 +209,18 @@ type View struct {
 
 type simpleResult struct {
   Ok bool `json:"ok"`
-  Id string `json:"id,omitempty"`
-  Rev string `json:"rev,omitempty"`
+  Id *string `json:"id,omitempty"`
+  Rev *string `json:"rev,omitempty"`
 }
 
 type errorResult struct {
-  Error string `json:"error"`
-  Reason string `json:"reason"`
+  Error *string `json:"error"`
+  Reason *string `json:"reason"`
 }
 
 type loginResult struct {
   Ok bool `json:"ok"`
-  Name string `json:"name"`
+  Name *string `json:"name"`
   Roles []string `json:"roles"`
 }
 
